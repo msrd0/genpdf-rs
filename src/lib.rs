0acbc7a904fbed9b3c35ba377b33c15774fe63e4
Dominic <git@msrd0.de>
2021-06-01 19:50:08 +0200

    implement horizontal layout for LinearLayout

--- src/lib.rs
+++ src/lib.rs
@@ -426,6 +426,17 @@ impl Size {
         self.height += other.height;
         self
     }
+    
+    /// Stacks the given size horizontally on this size and returns the result.
+    ///
+    /// This means that the height is set to the maximum of the heights and the width is set to the
+    /// sum of the widths.
+    #[must_use]
+    pub fn stack_horizontal(mut self, other: Size) -> Size {
+        self.height = self.height.max(other.height);
+        self.width += other.width;
+        self
+    }
 }
 
 impl<W: Into<Mm>, H: Into<Mm>> From<(W, H)> for Size {
